f060f06138a20732a31da7e4b68f510692101f5c
thembones79 <michal.szumnarski@gmail.con>
2024-05-05 12:27:01 +0200

    chunks

--- main.go
+++ main.go
@@ -1,50 +1,52 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
 	"fmt"
-	"log"
 	"os"
-	"strconv"
+	// "strconv"
+    // "strings"
+    "log"
+	"time"
 )
 
 func main() {
+
+	defer func(start time.Time) {
+		fmt.Printf("took us %v", time.Since(start))
+	}(time.Now())
+
 	f, err := os.Open("../measurements.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	scanner := bufio.NewScanner(f)
+    chunkSize := 500 * 1024 * 1024
+    for {
 
+    }
 
-    stations := map[string][ ]float64{}
-	for scanner.Scan() {
-		line := scanner.Text()
-        parts := strings.Split(line, ";")
-        station := parts[0]
-        temp, err := strconv.ParseFloat(parts[1], 64)
-        if err != nil {
-            log.Fatal(err)
-        }
+	// scanner := bufio.NewScanner(f)
 
+	// stations := map[string][]float64{}
+	// for scanner.Scan() {
+	// 	line := scanner.Text()
+	// 	parts := strings.Split(line, ";")
+	// 	station := parts[0]
+	// 	temp, err := strconv.ParseFloat(parts[1], 64)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	//
+ //        _ = station
+ //        _ = temp
 
-        if _,ok := stations[station]; !ok {
-           stations[station] = []float64(temp)
-        }else{
-stations[station] = append(stations[station],temp)
-        }
+		// if _, ok := stations[station]; !ok {
+		// 	stations[station] = []float64{temp}
+		// } else {
+		// 	stations[station] = append(stations[station], temp)
+		// }
 
-
-
-
-
-
-
-
-
-
-
-
-	}
+	// }
 
 }
